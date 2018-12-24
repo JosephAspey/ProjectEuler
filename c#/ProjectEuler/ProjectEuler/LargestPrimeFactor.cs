0d78c469f7503d8ef1cc00ba0e60b164857c6b6c
Joseph Aspey <joseph.aspey@betfred.com>
2018-12-24 15:42:37 +0000

    Test for prime factors - implementation incomplete

---
+++ c#/ProjectEuler/ProjectEuler/LargestPrimeFactor.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectEuler
+{
+    public class LargestPrimeFactor
+    {
+        public int Get(int valueToGetPrimeFactorsOf)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
