b3fee86c8046cb4f93d2c3308efab0c8be45b7a8
Joseph Aspey <joseph.aspey@betfred.com>
2018-12-24 11:27:44 +0000

    Multiples of 3 and 5

---
+++ c#/ProjectEuler/ProjectEuler.Tests/MultiplesOfThreeAndFiveTests.cs
@@ -0,0 +1,57 @@
+using System.Linq;
+using Xunit;
+
+namespace ProjectEuler.Tests
+{
+    /// <summary>
+    /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    /// Find the sum of all the multiples of 3 or 5 below 1000.
+    /// </summary>
+    public class MultiplesOfThreeAndFiveTests
+    {
+        [Fact]
+        public void ShouldReturnAllMultipleOfThreeAndFiveBelow1000()
+        {
+            //When
+            var result = MultiplesOfThreeAndFive.GetMultipleOfThreeAndFive(1000);
+
+            //Then
+            Assert.Contains(3, result);
+            Assert.Contains(5, result);
+            Assert.Contains(6, result);
+            Assert.Contains(9, result);
+        }
+
+        [Fact]
+        public void ShouldReturnNumberIfNumberIsAMultipleOfThree()
+        {
+            //When
+            var result = MultiplesOfThreeAndFive.GetMultipleOfThreeAndFive(3);
+
+            //Then
+            Assert.Contains(3, result);
+        }
+
+        [Fact]
+        public void ShouldReturnNumberIfNumberIsAMultipleOfFive()
+        {
+            //When
+            var result = MultiplesOfThreeAndFive.GetMultipleOfThreeAndFive(10);
+
+            //Then
+            Assert.Contains(5, result);
+        }
+
+        [Fact]
+        public void ShouldNotContainDuplicateMultipleFactors()
+        {
+            //When
+            var result = MultiplesOfThreeAndFive.GetMultipleOfThreeAndFive(15);
+
+            //Then
+            var distinctAmount = result.Distinct().Count();
+            Assert.Equal(distinctAmount, result.Count);
+        }
+
+    }
+}
---
+++ c#/ProjectEuler/ProjectEuler/MultiplesOfThreeAndFive.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace ProjectEuler
+{
+    public static class MultiplesOfThreeAndFive
+    {
+        public static List<int> GetMultipleOfThreeAndFive(int value)
+        {
+            var multiplesOfThreeAndFive = new List<int>();
+
+            for (var i = 1; i <= value; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    multiplesOfThreeAndFive.Add(i);
+                    continue;
+                }
+
+                if (i % 5 == 0)
+                {
+                    multiplesOfThreeAndFive.Add(i);
+                }
+            }
+
+            return multiplesOfThreeAndFive;
+        }
+    }
+}
