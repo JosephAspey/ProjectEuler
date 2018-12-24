ee296866417e48c40606ee1927be6fc58756d340
Joseph Aspey <joseph.aspey@betfred.com>
2018-12-24 12:19:07 +0000

    Even Fibonacci numbers sum

---
+++ c#/ProjectEuler/ProjectEuler/EvenFibonacciNumbersSum.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectEuler
+{
+    public class EvenFibonacciNumbersSum
+    {
+        public int Calculate(int maxNumber)
+        {
+            var fibonacciNumbers = GetFibonacciNumbers(maxNumber);
+            var evenFibonacciNumbers = GetEvenFibonacciNumbers(fibonacciNumbers);
+
+            var evenFibonacciNumbersSum = 0;
+            if (evenFibonacciNumbers.Any())
+            {
+                evenFibonacciNumbersSum = evenFibonacciNumbers.Sum();
+            }
+
+            return evenFibonacciNumbersSum;
+        }
+
+        private List<int> GetFibonacciNumbers(int maxNumber)
+        {
+            var fibonacciNumbers = new List<int>();
+
+            for (var i = 1; i < maxNumber; i++)
+            {
+                if (fibonacciNumbers.Any())
+                {
+                    var previousNumnerIndex = i - 2;
+                    var previousNumber = fibonacciNumbers[previousNumnerIndex];
+
+                    var previousNumber2Index = i - 3;
+                    var previousNumber2 = 1;
+                    if (previousNumber2Index >= 1)
+                    {
+                        previousNumber2 = fibonacciNumbers[previousNumber2Index];
+                    }
+
+                    var fibonacciNumber = previousNumber + previousNumber2;
+
+                    fibonacciNumbers.Add(fibonacciNumber);
+                }
+                else
+                {
+                    fibonacciNumbers.Add(1);
+                }
+            }
+
+            return fibonacciNumbers;
+        }
+
+        private static List<int> GetEvenFibonacciNumbers(IReadOnlyCollection<int> fibonacciNumbers)
+        {
+            var evenFibonacciNumbers = new List<int>();
+
+            if (!fibonacciNumbers.Any()) return evenFibonacciNumbers;
+
+            foreach (var fibonacciNumber in fibonacciNumbers)
+            {
+                if (fibonacciNumber % 2 == 0)
+                {
+                    evenFibonacciNumbers.Add(fibonacciNumber);
+                }
+            }
+
+            return evenFibonacciNumbers;
+        }
+       
+    }
+}
