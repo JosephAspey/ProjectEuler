ef630395c4b5e53db6478a4a90faa0e7848bc8bc
Joseph Aspey <joseph.aspey@betfred.com>
2019-01-29 10:37:02 +0000

    Problem 3 - Start of get largest prime factor of a number. Currently get the facotrs of a given number. Lots more left to do.

---
+++ c#/ProjectEuler/ProjectEuler.Tests/PrimeFactors/LargestPrimeFactorTests.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+
+namespace ProjectEuler.Tests
+{
+    /// <summary>
+    /// Problem 3
+    /// The prime factors of 13195 are 5, 7, 13 and 29.
+    /// What is the largest prime factor of the number 600851475143 ?
+    /// 
+    /// Help: https://www.mathsisfun.com/prime-factorization.html
+    ///
+    /// "Factors" are the numbers you multiply together to get another number:
+    /// 
+    /// A Prime Number can be divided exactly only by 1 or itself.
+    /// And it must be a whole number greater than 1.
+    /// </summary>
+    public class LargestPrimeFactorTests
+    {
+        [Fact]
+        public void ShouldGetFactorsOf10()
+        {
+            //Given
+            var sut = new LargestPrimeFactor();
+
+            //When
+            const int numberToGetFactorsOf = 10;
+            var result = sut.GetFactors(numberToGetFactorsOf);
+
+            //Then
+            Assert.Contains(1, result);
+            Assert.Contains(2, result);
+            Assert.Contains(5, result);
+            Assert.Contains(10, result);
+            Assert.True(result.Count == 4);
+        }
+
+        [Fact]
+        public void ShouldGetFactorsOf15()
+        {
+            //Given
+            var sut = new LargestPrimeFactor();
+
+            //When
+            const int numberToGetFactorsOf = 15;
+            var result = sut.GetFactors(numberToGetFactorsOf);
+
+            //Then
+            Assert.Contains(1, result);
+            Assert.Contains(3, result);
+            Assert.Contains(5, result);
+            Assert.Contains(15, result);
+            Assert.True(result.Count == 4);
+        }
+
+    }
+}
--- c#/ProjectEuler/ProjectEuler/LargestPrimeFactor.cs
+++ c#/ProjectEuler/ProjectEuler/LargestPrimeFactor.cs
@@ -1,10 +1,50 @@
-﻿namespace ProjectEuler
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler
 {
     public class LargestPrimeFactor
-    {
-        public int Get(int valueToGetPrimeFactorsOf)
+    { 
+        public int GetLargestPrimeFactor(int value)
         {
-            throw new System.NotImplementedException();
+
+            // get factors    
+            // 'factors' are number you multiply together to get another number
+
+            var factors = GetFactors(value);
+
+            //get prime factors from factor
+            var primeFators = GetPrimeFactors(factors);
+
+            return 5;
         }
+
+        public List<int> GetFactors(int value)
+        {
+            var factors = new List<int>();
+
+            for (var i = 0; i <= value; i++)
+            {
+                for (var j = 0; j <= value; j++)
+                {
+                    var sum = i * j;
+
+                    if (sum == value)
+                    {
+                        factors.Add(i);
+                    }
+                }
+            }
+
+            return factors;
+        }
+
+        public IEnumerable<int> GetPrimeFactors(IEnumerable<int> factors)
+        {
+            //Get prime numbers of factors
+
+            throw new NotImplementedException();
+        }
+
     }
 }
