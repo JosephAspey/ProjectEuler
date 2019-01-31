d49fbe6406af9b6f43a2f548748ac689a677a5d1
Joseph Aspey <joseph.aspey@betfred.com>
2019-01-31 15:54:45 +0000

    First step of probelm 4 - logic written with tests to identify a palindrome number

---
+++ c#/ProjectEuler/ProjectEuler.Tests/PalindromeProduct/LargestPalindromeProductTests.cs
@@ -0,0 +1,111 @@
+﻿using Xunit;
+
+namespace ProjectEuler.UnitTests.PalindromeProduct
+{
+    /// <summary>
+    /// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+    /// Find the largest palindrome made from the product of two 3-digit numbers.
+    /// </summary>
+    public class LargestPalindromeProductTests
+    {
+        //ToDo : Cleanup test to use constructor and pass in variables for same tests
+        [Fact]
+        public void ShouldReturnFalseWhenNumberIsNotPalindrome()
+        {
+            //Given
+            var sut = new ProjectEuler.PalindromeProduct.PalindromeProduct();
+
+            //When
+            const int number = 112;
+            var result = sut.IsPalindromeProduct(number);
+
+            //Then
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ShouldReturnFalseWhenNumberIsNotPalindromeFor1212()
+        {
+            //Given
+            var sut = new ProjectEuler.PalindromeProduct.PalindromeProduct();
+
+            //When
+            const int number = 1212;
+            var result = sut.IsPalindromeProduct(number);
+
+            //Then
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ShouldReturnFalseWhenNumberIsNotPalindromeFor12312()
+        {
+            //Given
+            var sut = new ProjectEuler.PalindromeProduct.PalindromeProduct();
+
+            //When
+            const int number = 12312;
+            var result = sut.IsPalindromeProduct(number);
+
+            //Then
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void ShouldReturnTrueWhenNumerIsPalindromeFor12321()
+        {
+            //Given
+            var sut = new ProjectEuler.PalindromeProduct.PalindromeProduct();
+
+            //When
+            const int number = 12321;
+            var result = sut.IsPalindromeProduct(number);
+
+            //Then
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ShouldReturnTrueWhenNumberIsPalindromFor121()
+        {
+            //Given
+            var sut = new ProjectEuler.PalindromeProduct.PalindromeProduct();
+
+            //When
+            const int number = 121;
+            var result = sut.IsPalindromeProduct(number);
+
+            //Then
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ShouldReturnTrueWhenNumberIsPalindromeFor1221()
+        {
+            //Given
+            var sut = new ProjectEuler.PalindromeProduct.PalindromeProduct();
+
+            //When
+            const int number = 1221;
+            var result = sut.IsPalindromeProduct(number);
+
+            //Then
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ShouldReturnFalseWhenSuppliedNumberIsOnlyOneDigit()
+        {
+            //Given
+            var sut = new ProjectEuler.PalindromeProduct.PalindromeProduct();
+
+            //When
+            const int number = 1;
+            var result = sut.IsPalindromeProduct(number);
+
+            //Then
+            Assert.False(result);
+        }
+
+    }
+}
---
+++ c#/ProjectEuler/ProjectEuler/PalindromeProduct/PalindromeProduct.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace ProjectEuler.PalindromeProduct
+{
+    public class PalindromeProduct
+    {
+        public bool IsPalindromeProduct(int number)
+        {
+            var numberLength = number.ToString().Length;
+
+            if (numberLength <= 1)
+            {
+                return false;
+            }
+
+            //split in centre
+            var productCenterLenght = (float)numberLength / 2;
+
+            if (productCenterLenght >= 1)
+            {
+                //reverse
+
+                //if odd length e.g. 3, 5 - round down and split
+                var isWholeNumber = Math.Abs(productCenterLenght % 1) < double.Epsilon;
+
+                string firstPartOfProduct = "";
+                string secondPartOfProduct = "";
+
+                if (!isWholeNumber)
+                {
+                    //round down to get first part - middle digit is disregarded
+                    var centerOfProductLengthRoundedDown = (int)Math.Floor(productCenterLenght);
+                    firstPartOfProduct = number.ToString().Substring(0 , centerOfProductLengthRoundedDown);
+                    
+                    //round up - to split and get after
+                    var centerOfProductRoundedUp = (int)Math.Ceiling(productCenterLenght);
+                    secondPartOfProduct = number.ToString().Substring(centerOfProductRoundedUp, centerOfProductLengthRoundedDown);
+                }
+                else
+                {
+                    firstPartOfProduct = number.ToString().Substring(0, (int)productCenterLenght);
+                    secondPartOfProduct =
+                        number.ToString().Substring((int) productCenterLenght, (int) productCenterLenght);
+                }
+               
+                //if string length > 2
+                var secondPartOfProductReversed = "";
+                if (secondPartOfProduct.Length >= 2)
+                {
+                    var arr = secondPartOfProduct.ToCharArray();
+                    Array.Reverse(arr);
+                    secondPartOfProductReversed = new string(arr);
+                }
+                else
+                {
+                    secondPartOfProductReversed = secondPartOfProduct;
+                }
+
+                if (firstPartOfProduct == secondPartOfProductReversed)
+                {
+                    return true;
+                }                        
+            }
+
+            return false;
+        }
+    }
+}
