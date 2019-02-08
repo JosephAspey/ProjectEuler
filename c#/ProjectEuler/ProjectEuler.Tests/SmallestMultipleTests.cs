61bb1036542a495682b925ff4b17e5d7b8138b5f
Joseph Aspey <joseph.aspey@betfred.com>
2019-02-08 16:17:20 +0000

    Attemp to start problem 5

---
+++ c#/ProjectEuler/ProjectEuler.Tests/SmallestMultipleTests.cs
@@ -0,0 +1,73 @@
+﻿using Xunit;
+
+namespace ProjectEuler.UnitTests
+{
+
+    /// <summary>
+    /// 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+    /// What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+    /// </summary>
+    public class SmallestMultipleTests
+    {
+        private readonly SmallestMultiple _sut;
+
+        public SmallestMultipleTests()
+        {
+            _sut = new SmallestMultiple();
+        }
+
+        [Fact]
+        public void ShouldReturnTrueWhenNumberIsEvenlyDivisibleWithoutAnyRemainder()
+        {
+            //Given        
+            const int numberToDivide = 2520;
+            const int numberToDivideBy = 2;
+
+            //When
+            var result = _sut.IsEvenlyDivisibleWithoutAnyRemainder(numberToDivide, numberToDivideBy);
+
+            //Then
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void ShouldReturnFalseWhenNumberIsNotEvenlyDivisbleWithoutAnyRemainer()
+        {
+            //Given
+            const int numberToDivide = 5;
+            const int numberToDivideBy = 3;
+
+            //When
+            var result = _sut.IsEvenlyDivisibleWithoutAnyRemainder(numberToDivide, numberToDivideBy);
+
+            //Then
+            Assert.False(result);
+        }
+
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+
+        public void ShouldReturnTrueAs2520CanBeDividedByEachNumberFrom1To10WithoutAnyRemainder(int numberToDivideBy)
+        {
+            //Given
+            const int numberToDivide = 2520;
+
+            //When
+            var reuslt = _sut.IsEvenlyDivisibleWithoutAnyRemainder(numberToDivide, numberToDivideBy);
+
+            //Then
+            Assert.True(reuslt);
+        }
+
+    }
+}
---
+++ c#/ProjectEuler/ProjectEuler/SmallestMultiple.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjectEuler
+{
+    public class SmallestMultiple
+    {
+        public bool IsEvenlyDivisibleWithoutAnyRemainder(int numberToDivide, int numberToDivideBy)
+        {
+
+            if (numberToDivide % numberToDivideBy  == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
